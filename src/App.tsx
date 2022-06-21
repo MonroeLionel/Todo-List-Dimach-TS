3e4496a9c3e962ee23f18dada435955ed6670374
MonroeLionel <MonroeLionel@gmail.com>
2022-06-21 17:54:36 +0300

    Todolist for students 02

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,68 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import {TuduList} from "./TuduList";
+import {tasks1PropsType, TuduList} from "./TuduList";
+
+// export function Counter() {
+//     console.log(`counter render`);
+//     let arr = useState(5);
+//     let data = arr[0];
+//     let setData = arr[1]
+//
+//     return <div onClick={() => {
+//         setData(data + 1)
+//     }}>{data}</div>
+// }
+export type FilterValueType = `all` | `completed` | `active`
 
 function App() {
-    const tasks1 = [
+    // let initTask = [
+    //     {id: 1, title: "HTML&CSS", isDone: true},
+    //     {id: 2, title: "JS", isDone: true},
+    //     {id: 3, title: "ReactJS", isDone: false},
+    //     {id: 4, title: "Redax", isDone: false}
+    // ]
+    //let arr = useState(initTask);
+    // let tasks1 = arr[0];
+    // let setTasks = arr[1];
+    let [tasks1, setTasks] = useState<Array<tasks1PropsType>>([
         {id: 1, title: "HTML&CSS", isDone: true},
         {id: 2, title: "JS", isDone: true},
         {id: 3, title: "ReactJS", isDone: false},
-        {id: 4, title: "ReactJS", isDone: false}
-    ]
-    const tasks2 = [
-        {id: 1, title: "Hello world", isDone: true},
-        {id: 2, title: "I am Happy", isDone: false},
-        {id: 3, title: "Yo", isDone: false}
-    ]
+        {id: 4, title: "Redax", isDone: false}
+    ]);
+    let [filter, sestFilter] = useState<FilterValueType>(`all`)
+
+    let taskForTodolist = tasks1;
+    if (filter === `completed`) {
+        taskForTodolist = tasks1.filter(el => el.isDone === true)
+    }
+    if (filter === `active`) {
+        taskForTodolist = tasks1.filter(el => el.isDone === false)
+    }
+    // const tasks2 = [
+    //     {id: 1, title: "Hello world", isDone: true},
+    //     {id: 2, title: "I am Happy", isDone: false},
+    //     {id: 3, title: "Yo", isDone: false}
+    // ]
+    function removeTask(id: number) {
+        let filteredTasks = tasks1.filter(el => el.id !== id)
+        setTasks(filteredTasks);
+    }
+
+    function changeFilter(value: FilterValueType) {
+        sestFilter(value);
+    }
 
     return (
         <div className="App">
-            <TuduList title={`What to learn`} tasks1={tasks1}/>
+            <TuduList
+                title={`What to learn`}
+                tasks1={taskForTodolist}
+                removeTasks={removeTask}
+                changeFilter={changeFilter}
+            />
 
-            <TuduList title={`What to buy`} tasks1={tasks2}/>
+            {/*<TuduList title={`What to buy`} tasks1={tasks2}/>*/}
 
         </div>
     );
--- src/TuduList.tsx
+++ src/TuduList.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import {FilterValueType} from "./App";
 
 type TuduListPropsType = {
     title: string
     title2?: boolean
     tasks1: Array<tasks1PropsType>
+    removeTasks: (id: number) => void
+    changeFilter: (value: FilterValueType) => void
 }
-type tasks1PropsType = {
+export type tasks1PropsType = {
     id: number,
     title: string,
     isDone: boolean
@@ -21,9 +24,18 @@ export function TuduList(props: TuduListPropsType) {
                 <button>+</button>
             </div>
             <ul>
+                {/*MAP это метод массива который на основе каждого объекта в массиве создает*/}
+                {/*какой то другой элемент*/}
+                {/*на выходе мы получаем новый массив с этими новыми элементами*/}
                 {props.tasks1.map(el => {
                     return (
-                        <li><input type="checkbox" checked={el.isDone}/> <span>{el.title}</span></li>
+                        <li><input type="checkbox" checked={el.isDone}/>
+                            <span>{el.title}</span>
+                            <button onClick={() => {
+                                props.removeTasks(el.id)
+                            }}>x
+                            </button>
+                        </li>
 
                     )
                 })}
@@ -31,9 +43,18 @@ export function TuduList(props: TuduListPropsType) {
 
             </ul>
             <div>
-                <button>All</button>
-                <button>Active</button>
-                <button>Completed</button>
+                <button onClick={() => {
+                    props.changeFilter(`all`)
+                }}>All
+                </button>
+                <button onClick={() => {
+                    props.changeFilter(`active`)
+                }}>Active
+                </button>
+                <button onClick={() => {
+                    props.changeFilter(`completed`)
+                }}>Completed
+                </button>
             </div>
         </div>
     )
